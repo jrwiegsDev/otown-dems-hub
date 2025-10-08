ec56de6e8099918fcdf8db43a01d20b5ab09974b
Joe <jrwiegs@gmail.com>
2025-10-08 18:09:47 -0500

    feat: fix stylesheet link

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,17 @@
 // src/main.jsx
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { ThemeProvider } from './context/ThemeContext'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { ThemeProvider } from './context/ThemeContext';
 
-// --- ADD THESE TWO LINES BACK ---
-import '@fullcalendar/core/main.css'
-import '@fullcalendar/daygrid/main.css'
+// DELETE the FullCalendar CSS imports from this file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
