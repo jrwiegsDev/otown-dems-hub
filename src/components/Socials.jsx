f2f8425ececd5493158d5409db37e5d1b2efec34
Joe Wiegert <jrwiegs@mac.lan>
2025-10-12 10:35:28 -0500

    feat: added email icon to socials

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,22 +1,24 @@
 // src/components/Socials.jsx
 
 import React from 'react';
-// Import the specific icons we need from the Font Awesome set
-import { FaFacebook, FaInstagram, FaTiktok } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaTiktok, FaEnvelope } from 'react-icons/fa';
 
 const Socials = () => {
   return (
     <div className="socials-component">
       <h3>Follow Us</h3>
       <div className="socials-icon-container">
-        <a href="https://www.facebook.com/ofallondemocrats" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.facebook.com/ofallondemocrats" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
           <FaFacebook />
         </a>
-        <a href="https://www.instagram.com/ofallon.dems" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/ofallon.dems" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
           <FaInstagram />
         </a>
-        <a href="https://www.tiktok.com/@ofallon.dems" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.tiktok.com/@ofallon.dems" target="_blank" rel="noopener noreferrer" aria-label="TikTok">
           <FaTiktok />
+        </a>
+        <a href="mailto:ofallondems@gmail.com" aria-label="Email">
+          <FaEnvelope />
         </a>
       </div>
     </div>
